d40e36f0d0f5f541742dbf542cd04670884ead68
Airto <99063870@mydavinci.nl>
2022-05-13 10:30:40 +0200

    inlog scherm
    je kan bijna inloggen

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,11 +5,10 @@ namespace App\Controllers;
 class Home extends BaseController{
 
     public function index(){
-        $model = new \App\Models\getSongs;
+        return view('Inloggen');
+    }
 
-        $muziek = $model->where("id", "1")
-                        ->first();
-
-        return view('Inloggen.php', ["muziek" => $muziek]);
+    public function createAccount($create){
+        return view("inloggen", ["create" => $create]);
     }
 }
--- app/Views/Inloggen.php
+++ app/Views/Inloggen.php
@@ -1,18 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
-    <title>InlogScherm</title>
-</head>
-<body>
-    <header>vdsgewgfs</header>
+<?= $this->include('layouts/header') ?>
 
-<p>grieojgiojgijos</p>
-<br>
-<?php var_dump($muziek["nameSong"]); ?>
+    <div class="inloggen">
+        <h1 class="hoofdLetters">inloggen</h1>
+        <?= form_open("/Admin/Users/update/") ?>
+            <label class="label" for="Name">Name</label>
+            <input class="input" type="text" name="Name">
+
+            <label class="label" for="Name">Password</label>
+            <input class="input" type="password" name="Password">
+
+            <div class="centerV">
+                <button class="hoofdLetters">inloggen</button>
+            </div>
+        </form>
+    </div>
 
 </body>
 </html>
+
+<script>
+    
+</script>
